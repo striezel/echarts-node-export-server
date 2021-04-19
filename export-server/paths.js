b211d384965d34fbf9f9f2fd39a4e580d015a0bf
Dirk Stolle <striezel-dev@web.de>
2021-04-20 01:34:59 +0200

    update year in paths.js

--- export-server/paths.js
+++ export-server/paths.js
@@ -1,6 +1,6 @@
 /*
     ECharts offline image export server with Node.js
-    Copyright (C) 2018, 2020  Dirk Stolle
+    Copyright (C) 2018, 2020, 2021  Dirk Stolle
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
