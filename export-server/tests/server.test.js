af02fc5d69654a7a2125a0fef3e548c84f6f2cb2
Dirk Stolle <striezel-dev@web.de>
2024-12-30 23:12:55 +0100

    tests: add license notice to server test file

--- export-server/tests/server.test.js
+++ export-server/tests/server.test.js
@@ -1,3 +1,21 @@
+/*
+    ECharts offline image export server with Node.js test suite
+    Copyright (C) 2023, 2024  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 const assert = require('node:assert');
 const http = require('node:http');
 const { describe, it } = require('node:test');
