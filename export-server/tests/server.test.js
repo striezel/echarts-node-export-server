115f6b6f3d23892e927ae9e123b3c13e56daba60
Dirk Stolle <striezel-dev@web.de>
2025-01-01 18:57:45 +0100

    tests: add small test for PNG image generation

--- export-server/tests/server.test.js
+++ export-server/tests/server.test.js
@@ -1,6 +1,6 @@
 /*
     ECharts offline image export server with Node.js test suite
-    Copyright (C) 2023, 2024  Dirk Stolle
+    Copyright (C) 2023, 2024, 2025  Dirk Stolle
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -17,6 +17,7 @@
 */
 
 const assert = require('node:assert');
+const { Buffer } = require('node:buffer');
 const http = require('node:http');
 const { describe, it } = require('node:test');
 
@@ -255,6 +256,41 @@ describe('server', () => {
           assert.ok(body.indexOf('pants') > 0);
           assert.ok(body.indexOf('heels') > 0);
           assert.ok(body.indexOf('socks') > 0);
+        });
+      });
+    });
+
+    it('request with example data and PNG format is successful', () => {
+      const options = {
+        port: 3000,
+        host: 'localhost',
+        method: 'POST'
+      };
+
+      const req = http.request(options);
+      req.write(payload);
+      req.end();
+
+      req.on('response', (response) => {
+        assert.strictEqual(200, response.statusCode);
+        let body = Buffer.alloc(0);
+        response.on('data', (chunk) => {
+          body = Buffer.concat([body, chunk]);
+        });
+        response.on('end', () => {
+          // This is only a very simple check for PNG output.
+          // Since the same image data can be represented in different ways in
+          // PNG files, this tests only checks the start, the end and some known
+          // fixed value in between.
+          assert.strictEqual(0x89, body[0]);
+          assert.strictEqual(0x50, body[1]); // 'P'
+          assert.strictEqual(0x4E, body[2]); // 'N'
+          assert.strictEqual(0x47, body[3]); // 'G'
+          const startBuffer = Buffer.from('\x89PNG\r\n\x1A\n\0\0\0\x0DIHDR', 'binary');
+          assert.strictEqual(0, body.compare(startBuffer, 0, startBuffer.length, 0, startBuffer.length));
+          const endBuffer = Buffer.from('IEND\xAEB\x60\x82', 'binary');
+          assert.strictEqual(0, body.compare(endBuffer, 0, endBuffer.length, body.length - endBuffer.length, body.length));
+          assert.ok(body.includes('IDAT'));
         });
       });
     });
