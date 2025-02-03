ca72b61adf8abe0e17cc62f5a6d554430feb2d11
Dirk Stolle <striezel-dev@web.de>
2025-02-03 10:47:31 +0100

    tests: add test for 'Bar with Background' example

--- export-server/tests/charts/bar.test.js
+++ export-server/tests/charts/bar.test.js
@@ -379,8 +379,7 @@ fill:rgba(92,123,217,1);
     });
   });
 
-/*
-  it('request with different data', () => {
+  it('Bar with Background', () => {
     const options = {
       port: 3000,
       host: 'localhost',
@@ -391,7 +390,27 @@ fill:rgba(92,123,217,1);
     };
 
     const req = http.request(options);
-    const payload = '...';
+    // Data taken from https://echarts.apache.org/examples/en/editor.html?c=bar-background example.
+    const payload = `{
+                       "xAxis": {
+                         "type": "category",
+                         "data": [ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" ]
+                       },
+                       "yAxis": {
+                         "type": "value"
+                       },
+                       "series": [
+                         {
+                           "data": [ 120, 200, 150, 80, 70, 110, 130 ],
+                           "type": "bar",
+                           "showBackground": true,
+                           "backgroundStyle": {
+                             "color": "rgba(180, 180, 180, 0.2)"
+                           }
+                         }
+                       ],
+                       "backgroundColor": "#ffffff"
+                     }`;
     req.write(payload);
     req.end();
 
@@ -404,9 +423,194 @@ fill:rgba(92,123,217,1);
       response.on('end', () => {
         assert.ok(body.startsWith('<svg'));
         assert.ok(body.endsWith('</svg>'));
+
+        /* Current server response is something like
+
+        <svg width="700" height="400" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" baseProfile="full" viewBox="0 0 700 400">
+<rect width="700" height="400" x="0" y="0" fill="#ffffff"></rect>
+<path d="M70 330.5L630 330.5" fill="none" pointer-events="visible" stroke="#E0E6F1" class="zr0-cls-0"></path>
+<path d="M70 262.5L630 262.5" fill="none" pointer-events="visible" stroke="#E0E6F1" class="zr0-cls-0"></path>
+<path d="M70 195.5L630 195.5" fill="none" pointer-events="visible" stroke="#E0E6F1" class="zr0-cls-0"></path>
+<path d="M70 127.5L630 127.5" fill="none" pointer-events="visible" stroke="#E0E6F1" class="zr0-cls-0"></path>
+<path d="M70 60.5L630 60.5" fill="none" pointer-events="visible" stroke="#E0E6F1" class="zr0-cls-0"></path>
+<path d="M70 330.5L630 330.5" fill="none" pointer-events="visible" stroke="#6E7079" stroke-linecap="round" class="zr0-cls-0"></path>
+<path d="M70.5 330L70.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="zr0-cls-0"></path>
+<path d="M150.5 330L150.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="zr0-cls-0"></path>
+<path d="M230.5 330L230.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="zr0-cls-0"></path>
+<path d="M310.5 330L310.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="zr0-cls-0"></path>
+<path d="M390.5 330L390.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="zr0-cls-0"></path>
+<path d="M470.5 330L470.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="zr0-cls-0"></path>
+<path d="M550.5 330L550.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="zr0-cls-0"></path>
+<path d="M630.5 330L630.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="zr0-cls-0"></path>
+<text dominant-baseline="central" text-anchor="end" style="font-size:12px;font-family:sans-serif;" transform="translate(62 330)" fill="#6E7079">0</text>
+<text dominant-baseline="central" text-anchor="end" style="font-size:12px;font-family:sans-serif;" transform="translate(62 262.5)" fill="#6E7079">50</text>
+<text dominant-baseline="central" text-anchor="end" style="font-size:12px;font-family:sans-serif;" transform="translate(62 195)" fill="#6E7079">100</text>
+<text dominant-baseline="central" text-anchor="end" style="font-size:12px;font-family:sans-serif;" transform="translate(62 127.5)" fill="#6E7079">150</text>
+<text dominant-baseline="central" text-anchor="end" style="font-size:12px;font-family:sans-serif;" transform="translate(62 60)" fill="#6E7079">200</text>
+<text dominant-baseline="central" text-anchor="middle" style="font-size:12px;font-family:sans-serif;" y="6" transform="translate(110 338)" fill="#6E7079">Mon</text>
+<text dominant-baseline="central" text-anchor="middle" style="font-size:12px;font-family:sans-serif;" y="6" transform="translate(190 338)" fill="#6E7079">Tue</text>
+<text dominant-baseline="central" text-anchor="middle" style="font-size:12px;font-family:sans-serif;" y="6" transform="translate(270 338)" fill="#6E7079">Wed</text>
+<text dominant-baseline="central" text-anchor="middle" style="font-size:12px;font-family:sans-serif;" y="6" transform="translate(350 338)" fill="#6E7079">Thu</text>
+<text dominant-baseline="central" text-anchor="middle" style="font-size:12px;font-family:sans-serif;" y="6" transform="translate(430 338)" fill="#6E7079">Fri</text>
+<text dominant-baseline="central" text-anchor="middle" style="font-size:12px;font-family:sans-serif;" y="6" transform="translate(510 338)" fill="#6E7079">Sat</text>
+<text dominant-baseline="central" text-anchor="middle" style="font-size:12px;font-family:sans-serif;" y="6" transform="translate(590 338)" fill="#6E7079">Sun</text>
+<path d="M82.4 60l55.2 0l0 270l-55.2 0Z" fill="rgb(180,180,180)" fill-opacity="0.2" class="zr0-cls-0"></path>
+<path d="M162.4 60l55.2 0l0 270l-55.2 0Z" fill="rgb(180,180,180)" fill-opacity="0.2" class="zr0-cls-0"></path>
+<path d="M242.4 60l55.2 0l0 270l-55.2 0Z" fill="rgb(180,180,180)" fill-opacity="0.2" class="zr0-cls-0"></path>
+<path d="M322.4 60l55.2 0l0 270l-55.2 0Z" fill="rgb(180,180,180)" fill-opacity="0.2" class="zr0-cls-0"></path>
+<path d="M402.4 60l55.2 0l0 270l-55.2 0Z" fill="rgb(180,180,180)" fill-opacity="0.2" class="zr0-cls-0"></path>
+<path d="M482.4 60l55.2 0l0 270l-55.2 0Z" fill="rgb(180,180,180)" fill-opacity="0.2" class="zr0-cls-0"></path>
+<path d="M562.4 60l55.2 0l0 270l-55.2 0Z" fill="rgb(180,180,180)" fill-opacity="0.2" class="zr0-cls-0"></path>
+<path d="M82.4 330l55.2 0l0 -162l-55.2 0Z" fill="#5470c6" ecmeta_series_index="0" ecmeta_data_index="0" ecmeta_ssr_type="chart" class="zr0-cls-1"></path>
+<path d="M162.4 330l55.2 0l0 -270l-55.2 0Z" fill="#5470c6" ecmeta_series_index="0" ecmeta_data_index="1" ecmeta_ssr_type="chart" class="zr0-cls-1"></path>
+<path d="M242.4 330l55.2 0l0 -202.5l-55.2 0Z" fill="#5470c6" ecmeta_series_index="0" ecmeta_data_index="2" ecmeta_ssr_type="chart" class="zr0-cls-1"></path>
+<path d="M322.4 330l55.2 0l0 -108l-55.2 0Z" fill="#5470c6" ecmeta_series_index="0" ecmeta_data_index="3" ecmeta_ssr_type="chart" class="zr0-cls-1"></path>
+<path d="M402.4 330l55.2 0l0 -94.5l-55.2 0Z" fill="#5470c6" ecmeta_series_index="0" ecmeta_data_index="4" ecmeta_ssr_type="chart" class="zr0-cls-1"></path>
+<path d="M482.4 330l55.2 0l0 -148.5l-55.2 0Z" fill="#5470c6" ecmeta_series_index="0" ecmeta_data_index="5" ecmeta_ssr_type="chart" class="zr0-cls-1"></path>
+<path d="M562.4 330l55.2 0l0 -175.5l-55.2 0Z" fill="#5470c6" ecmeta_series_index="0" ecmeta_data_index="6" ecmeta_ssr_type="chart" class="zr0-cls-1"></path>
+<style ><![CDATA[
+.zr0-cls-0:hover {
+pointer-events:none;
+}
+.zr0-cls-1:hover {
+cursor:pointer;
+fill:rgba(92,123,217,1);
+}
+
+]]>
+
+</style>
+</svg>
+
+        */
+
+        // Check partial matches, avoiding the random class / id bits.
+        assert.ok(body.indexOf('<svg width="700" height="400" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" baseProfile="full" viewBox="0 0 700 400">') >= 0);
+        assert.ok(body.indexOf(`<rect width="700" height="400" x="0" y="0" fill="#ffffff"></rect>
+<path d="M70 330.5L630 330.5" fill="none" pointer-events="visible" stroke="#E0E6F1" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M70 262.5L630 262.5" fill="none" pointer-events="visible" stroke="#E0E6F1" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M70 195.5L630 195.5" fill="none" pointer-events="visible" stroke="#E0E6F1" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M70 127.5L630 127.5" fill="none" pointer-events="visible" stroke="#E0E6F1" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M70 60.5L630 60.5" fill="none" pointer-events="visible" stroke="#E0E6F1" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M70 330.5L630 330.5" fill="none" pointer-events="visible" stroke="#6E7079" stroke-linecap="round" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M70.5 330L70.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M150.5 330L150.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M230.5 330L230.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M310.5 330L310.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M390.5 330L390.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M470.5 330L470.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M550.5 330L550.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M630.5 330L630.5 335" fill="none" pointer-events="visible" stroke="#6E7079" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<text dominant-baseline="central" text-anchor="end" style="font-size:12px;font-family:sans-serif;" transform="translate(62 330)" fill="#6E7079">0</text>
+<text dominant-baseline="central" text-anchor="end" style="font-size:12px;font-family:sans-serif;" transform="translate(62 262.5)" fill="#6E7079">50</text>
+<text dominant-baseline="central" text-anchor="end" style="font-size:12px;font-family:sans-serif;" transform="translate(62 195)" fill="#6E7079">100</text>
+<text dominant-baseline="central" text-anchor="end" style="font-size:12px;font-family:sans-serif;" transform="translate(62 127.5)" fill="#6E7079">150</text>
+<text dominant-baseline="central" text-anchor="end" style="font-size:12px;font-family:sans-serif;" transform="translate(62 60)" fill="#6E7079">200</text>
+<text dominant-baseline="central" text-anchor="middle" style="font-size:12px;font-family:sans-serif;" y="6" transform="translate(110 338)" fill="#6E7079">Mon</text>
+<text dominant-baseline="central" text-anchor="middle" style="font-size:12px;font-family:sans-serif;" y="6" transform="translate(190 338)" fill="#6E7079">Tue</text>
+<text dominant-baseline="central" text-anchor="middle" style="font-size:12px;font-family:sans-serif;" y="6" transform="translate(270 338)" fill="#6E7079">Wed</text>
+<text dominant-baseline="central" text-anchor="middle" style="font-size:12px;font-family:sans-serif;" y="6" transform="translate(350 338)" fill="#6E7079">Thu</text>
+<text dominant-baseline="central" text-anchor="middle" style="font-size:12px;font-family:sans-serif;" y="6" transform="translate(430 338)" fill="#6E7079">Fri</text>
+<text dominant-baseline="central" text-anchor="middle" style="font-size:12px;font-family:sans-serif;" y="6" transform="translate(510 338)" fill="#6E7079">Sat</text>
+<text dominant-baseline="central" text-anchor="middle" style="font-size:12px;font-family:sans-serif;" y="6" transform="translate(590 338)" fill="#6E7079">Sun</text>
+<path d="M82.4 60l55.2 0l0 270l-55.2 0Z" fill="rgb(180,180,180)" fill-opacity="0.2" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M162.4 60l55.2 0l0 270l-55.2 0Z" fill="rgb(180,180,180)" fill-opacity="0.2" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M242.4 60l55.2 0l0 270l-55.2 0Z" fill="rgb(180,180,180)" fill-opacity="0.2" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M322.4 60l55.2 0l0 270l-55.2 0Z" fill="rgb(180,180,180)" fill-opacity="0.2" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M402.4 60l55.2 0l0 270l-55.2 0Z" fill="rgb(180,180,180)" fill-opacity="0.2" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M482.4 60l55.2 0l0 270l-55.2 0Z" fill="rgb(180,180,180)" fill-opacity="0.2" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M562.4 60l55.2 0l0 270l-55.2 0Z" fill="rgb(180,180,180)" fill-opacity="0.2" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M82.4 330l55.2 0l0 -162l-55.2 0Z" fill="#5470c6" ecmeta_series_index="0" ecmeta_data_index="0" ecmeta_ssr_type="chart" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M162.4 330l55.2 0l0 -270l-55.2 0Z" fill="#5470c6" ecmeta_series_index="0" ecmeta_data_index="1" ecmeta_ssr_type="chart" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M242.4 330l55.2 0l0 -202.5l-55.2 0Z" fill="#5470c6" ecmeta_series_index="0" ecmeta_data_index="2" ecmeta_ssr_type="chart" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M322.4 330l55.2 0l0 -108l-55.2 0Z" fill="#5470c6" ecmeta_series_index="0" ecmeta_data_index="3" ecmeta_ssr_type="chart" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M402.4 330l55.2 0l0 -94.5l-55.2 0Z" fill="#5470c6" ecmeta_series_index="0" ecmeta_data_index="4" ecmeta_ssr_type="chart" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M482.4 330l55.2 0l0 -148.5l-55.2 0Z" fill="#5470c6" ecmeta_series_index="0" ecmeta_data_index="5" ecmeta_ssr_type="chart" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<path d="M562.4 330l55.2 0l0 -175.5l-55.2 0Z" fill="#5470c6" ecmeta_series_index="0" ecmeta_data_index="6" ecmeta_ssr_type="chart" class="`) > 0);
+        assert.ok(body.indexOf(`"></path>
+<style ><![CDATA[
+.`) > 0);
+        assert.ok(body.indexOf(`:hover {
+pointer-events:none;
+}
+.`) > 0);
+        assert.ok(body.indexOf(`:hover {
+cursor:pointer;
+fill:rgba(92,123,217,1);
+}
+
+]]>
+
+</style>
+</svg>`) > 0);
+      });
+    });
+  });
+
+/*
+  it('request with different data', () => {
+    const options = {
+      port: 3000,
+      host: 'localhost',
+      method: 'POST',
+      headers: {
+        'X-Image-Format': 'svg'
+      }
+    };
+
+    const req = http.request(options);
+    // Data taken from https://echarts.apache.org/examples/en/editor.html?c=... example.
+    const payload = `...`;
+    req.write(payload);
+    req.end();
+
+    req.on('response', (response) => {
+      assert.strictEqual(200, response.statusCode);
+      let body = '';
+      response.on('data', (chunk) => {
+        body += chunk;
+      });
+      response.on('end', () => {
+        assert.ok(body.startsWith('<svg'));
+        assert.ok(body.endsWith('</svg>'));
+
+        // Current server response is something like
+
+        <svg ...
+
+        //
+
+        // Check partial matches, avoiding the random class / id bits.
         assert.ok(body.indexOf('') >= 0);
-        assert.ok(body.indexOf('') > 0);
-        assert.ok(body.indexOf('') > 0);
+        assert.ok(body.indexOf(``) > 0);
+        assert.ok(body.indexOf(``) > 0);
       });
     });
   });
